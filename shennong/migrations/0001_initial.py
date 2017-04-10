3ae2dcc46a469e47bd88e60d3fb567a14b451459
BobBowles <bobjohnbowles@gmail.com>
2017-04-10 13:20:57 +0100

    Define data model

---
+++ shennong/migrations/0001_initial.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Herb',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', primary_key=True, serialize=False, auto_created=True)),
+                ('chinese', models.CharField(max_length=20)),
+                ('pinyin', models.CharField(max_length=50)),
+                ('latin', models.CharField(max_length=200)),
+                ('english', models.CharField(max_length=200)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Ingredient',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', primary_key=True, serialize=False, auto_created=True)),
+                ('herb', models.ForeignKey(to='shennong.Herb')),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Recipe',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', primary_key=True, serialize=False, auto_created=True)),
+                ('chinese', models.CharField(max_length=20)),
+                ('pinyin', models.CharField(max_length=50)),
+                ('english', models.CharField(max_length=200)),
+                ('ingredients', models.ManyToManyField(to='shennong.Herb', through='shennong.Ingredient')),
+            ],
+        ),
+        migrations.AddField(
+            model_name='ingredient',
+            name='recipe',
+            field=models.ForeignKey(to='shennong.Recipe'),
+        ),
+    ]
--- shennong/models.py
+++ shennong/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Herb(models.Model):
+    """
+    A herb describes a plant from the canon of Chinese herbs. It provides
+    the herb's Chinese name, the name in Pinyin, the Latin name, and common 
+    English name.
+    """
+    
+    chinese = models.CharField(max_length=20)
+    pinyin = models.CharField(max_length=50)
+    latin = models.CharField(max_length=200)
+    english = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.pinyin
+
+
+class Recipe(models.Model):
+    """
+    A recipe includes the list of herbs to use for a particular treatment. The 
+    recipe has a Chinese name, a Pinyin name, and an English description.
+    """
+    
+    chinese = models.CharField(max_length=20)
+    pinyin = models.CharField(max_length=50)
+    english = models.CharField(max_length=200)
+    ingredients = models.ManyToManyField(Herb, through='Ingredient')
+    
+    def __str__(self):
+        return self.pinyin
+
+
+class Ingredient(models.Model):
+    """
+    An ingredient cross-references herbs and recipes.
+    TODO: may include details of proportions/amounts of a herb in a recipe.
+    """
+    
+    recipe = models.ForeignKey(Recipe)
+    herb = models.ForeignKey(Herb)
+    
+    def __str__(self):
+        return self.recipe + ' contains ' + self.herb
