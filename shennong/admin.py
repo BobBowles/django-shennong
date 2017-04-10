ecb17ef7ad479ed3da84172f531b5af27de4ceec
BobBowles <bobjohnbowles@gmail.com>
2017-04-10 13:28:29 +0100

    Add admin ui

--- shennong/admin.py
+++ shennong/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Herb, Recipe, Ingredient
+
+
+admin.site.register(Herb)
+admin.site.register(Recipe)
+admin.site.register(Ingredient)
+
