3c3c9b00a34048d24ad24ae5337a502e8d0672bc
BobBowles <bobjohnbowles@gmail.com>
2017-04-11 14:03:20 +0100

    Set up basic navigation.

--- shennong/urls.py
+++ shennong/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^(?P<id>[0-9]+)/$', views.recipe_header, name="header"),
+    url(r'^(?P<id>[0-9]+)/full/$', views.recipe_full, name="full"),
+    url(r'^(?P<id>[0-9]+)/herb/$', views.herb, name="herb"),
 ]
--- shennong/views.py
+++ shennong/views.py
@@ -2,9 +2,26 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from .models import Recipe, Herb
 
 
 def index(request):
-    return HttpResponse("Hello Shennong World!")
+    recipe_index = Recipe.objects.order_by('pinyin')
+    output = ', '.join([recipe.pinyin for recipe in recipe_index])
+    return HttpResponse(output)
 
+
+def recipe_header(request, id):
+    response = "Header Description of Recipe {}."
+    return HttpResponse(response.format(id))
+
+
+def recipe_full(request, id):
+    response = "Full Description of Recipe {}."
+    return HttpResponse(response.format(id))
+
+
+def herb(request, id):
+    response = "Description of Herb {}."
+    return HttpResponse(response.format(id))
 
