5cbc3ce3d327dd7b8c2c757315c1d2bf58476c1b
BobBowles <bobjohnbowles@gmail.com>
2017-04-11 16:31:50 +0100

    Add basic views and templates.

--- shennong/views.py
+++ shennong/views.py
@@ -1,27 +1,49 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 # Create your views here.
 from django.http import HttpResponse
+from django.template import loader
 from .models import Recipe, Herb
 
 
 def index(request):
+    """
+    Display a list of recipes to select from.
+    """
     recipe_index = Recipe.objects.order_by('pinyin')
-    output = ', '.join([recipe.pinyin for recipe in recipe_index])
-    return HttpResponse(output)
+#    template = loader.get_template('shennong/index.html')
+    context = {
+        'recipe_index': recipe_index,
+    }
+#    return HttpResponse(template.render(context, request))
+    return render(request, 'shennong/index.html', context)
 
 
 def recipe_header(request, id):
-    response = "Header Description of Recipe {}."
-    return HttpResponse(response.format(id))
+    """
+    Display header data for a recipe.
+    """
+    recipe = get_object_or_404(Recipe, pk=id)
+    
+    return render(request, 'shennong/recipe_header.html', {'recipe': recipe,})
+
+
 
 
 def recipe_full(request, id):
-    response = "Full Description of Recipe {}."
-    return HttpResponse(response.format(id))
+    """
+    Display full data for a recipe.
+    """
+    recipe = get_object_or_404(Recipe, pk=id)
+    
+    return render(request, 'shennong/recipe_full.html', {'recipe': recipe,})
 
 
 def herb(request, id):
-    response = "Description of Herb {}."
-    return HttpResponse(response.format(id))
+    """
+    Display data for an herb.
+    """
+    herb = get_object_or_404(Herb, pk=id)
+    
+    return render(request, 'shennong/herb.html', {'herb': herb,})
 
