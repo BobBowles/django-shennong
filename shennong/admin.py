5ef45071561c1f859da78c0dceeb538b9ec3aed6
BobBowles <bobjohnbowles@gmail.com>
2017-04-11 09:01:30 +0100

    Beautify admin list displays.

--- shennong/admin.py
+++ shennong/admin.py
@@ -4,12 +4,16 @@ from django.contrib import admin
 from .models import Herb, Recipe, Ingredient
 
 
-#class HerbAdmin(admin.ModelAdmin):
-#    pass
+class HerbAdmin(admin.ModelAdmin):
+    """
+    Tailor the list display of herb items on the admin form.
+    """
+    
+    list_display = ('chinese', 'pinyin', 'latin', 'english')
 
 
-#class IngredientAdmin(admin.ModelAdmin):
-#    pass
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ('recipe', 'herb')
 
 
 class IngredientInline(admin.StackedInline):
@@ -24,16 +28,17 @@ class IngredientInline(admin.StackedInline):
 class RecipeAdmin(admin.ModelAdmin):
     """
     Allow for recipe ingredients to be added on the admin form.
+    Tailor the list display of recipe information on the admin form.
     """
+    
+    list_display = ('chinese', 'pinyin', 'english')
     
     # TODO: filter_horizontal does not seem to work as expected
     inlines = [IngredientInline]
     #filter_horizontal = ['ingredients',]
 
 
-admin.site.register(Herb)
-#admin.site.register(Herb, HerbAdmin)
+admin.site.register(Herb, HerbAdmin)
 admin.site.register(Recipe, RecipeAdmin)
-admin.site.register(Ingredient)
-#admin.site.register(Ingredient, IngredientAdmin)
+admin.site.register(Ingredient, IngredientAdmin)
 
