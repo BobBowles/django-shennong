c84d4df96f324e1459027ee19436110b8a5c6376
BobBowles <bobjohnbowles@gmail.com>
2017-04-15 17:48:53 +0100

    Fix url for recipe header.

--- shennong/urls.py
+++ shennong/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^(?P<id>[0-9]+)/$', views.recipe_header, name='recipe_header'),
+    url(r'^(?P<id>[0-9]+)$', views.recipe_header, name='recipe_header'),
     url(r'^(?P<id>[0-9]+)/full/$', views.recipe_full, name='recipe_full'),
     url(r'^(?P<id>[0-9]+)/herb/$', views.herb, name='herb'),
 ]
